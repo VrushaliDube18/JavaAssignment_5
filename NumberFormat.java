e13fb2d1ee79b393585835f536a1a01fc2f7b5af
VrushaliDube18 <vrushalidube18@gmail.com>
2025-09-11 15:31:11 +0530

    Add files via upload

---
+++ NumberFormat.java
@@ -0,0 +1,8 @@
+class NumberFormat
+{
+public static void main(String args[])
+{
+int num = Integer.parseInt("Vrushali");
+System.out.println(num);
+}
+}
---
+++ NumberFormat1.java
@@ -0,0 +1,15 @@
+class NumberFormat1
+{
+public static void main(String args[])
+{
+try
+{
+int num = Integer.parseInt("Vrushali");
+System.out.println(num);
+}
+catch(Exception e)
+{
+System.out.println("Cannot convert Character string to Integer");
+}
+}
+}
